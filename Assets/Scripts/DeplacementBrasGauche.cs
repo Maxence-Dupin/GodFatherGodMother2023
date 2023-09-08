23c4a036119e2e54c27638ee68b1f70199663b67
maxence-dupin <maxencedupin001@gmail.com>
2023-09-08 10:23:57 +0200

    Fix deplacements

--- Assets/Scripts/DeplacementBrasGauche.cs
+++ Assets/Scripts/DeplacementBrasGauche.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DeplacementBrasGauche : MonoBehaviour
@@ -16,7 +14,7 @@ public class DeplacementBrasGauche : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
