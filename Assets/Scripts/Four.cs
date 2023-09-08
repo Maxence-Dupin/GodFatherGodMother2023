32c216101cb6e5ff1205bca098c2ca589094b264
maxence-dupin <maxencedupin001@gmail.com>
2023-09-08 11:24:59 +0200

    Fix four

--- Assets/Scripts/Four.cs
+++ Assets/Scripts/Four.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Four : MonoBehaviour
@@ -7,6 +8,7 @@ public class Four : MonoBehaviour
     [SerializeField] private Bowl _bowl;
     [SerializeField] private Rigidbody _bowlRb;
     [SerializeField] private BoxCollider _bowlTrigger;
+    [SerializeField] private BoxCollider _bowlCollision;
     [SerializeField] private Transform _bowlInTransform;
     [SerializeField] private SkinnedMeshRenderer _fourSMR;
 
@@ -71,9 +73,10 @@ public class Four : MonoBehaviour
               (_rightHandInTrigger && Input.GetKeyDown(InputManager.Instance.RightJoystickButton1) )))
         {
             _bowlRb.transform.position = _bowlOutTransform.position;
-            _bowlRb.isKinematic = false;
-            _bowlTrigger.enabled = true;
             
+            _bowlRb.isKinematic = false;
+            StartCoroutine(WaitCollision());
+
             _bowlIn = false;
         }
 
@@ -89,6 +92,13 @@ public class Four : MonoBehaviour
         }
     }
 
+    IEnumerator WaitCollision()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _bowlTrigger.enabled = true;
+        _bowlCollision.enabled = true;
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.GetComponent<Bowl>() && !_closed)
@@ -99,7 +109,8 @@ public class Four : MonoBehaviour
                 
                 _bowlRb.isKinematic = true;
                 _bowlTrigger.enabled = false;
-                
+                _bowlCollision.enabled = false;
+
                 _bowlIn = true;
                 _bowlRb.gameObject.transform.position = _bowlInTransform.position;
             }
