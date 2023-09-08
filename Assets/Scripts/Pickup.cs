7f71076105b403f1e24d6de5b505a6e821e725ab
illianes <pierremariotte98@gmail.com>
2023-09-08 11:15:54 +0200

    MinigameFix

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,10 +5,11 @@ public class Pickup : MonoBehaviour
     [Header("Settings")]
     [SerializeField] Transform holdArea;
     [SerializeField] private bool _leftArm;
-    
+
     private GameObject heldObject;
     private Rigidbody rb;
 
+    public Pickup otherArm;
     public Minigame minigame;
     private bool bowl, whisk = false;
     [Header("Physics")]
@@ -17,10 +18,10 @@ public class Pickup : MonoBehaviour
 
     private void Update()
     {
-        if ((_leftArm && Input.GetKeyDown(InputManager.Instance.LeftJoystickButton1)) 
+        if ((_leftArm && Input.GetKeyDown(InputManager.Instance.LeftJoystickButton1))
         || (!_leftArm && Input.GetKeyDown(InputManager.Instance.RightJoystickButton1)))
         {
-            if (heldObject == null) 
+            if (heldObject == null)
             {
                 RaycastHit hit;
                 if (Physics.Raycast(new Ray(holdArea.position, Vector3.forward), out hit, armRange))
@@ -39,21 +40,28 @@ public class Pickup : MonoBehaviour
         }
     }
 
-    void MoveObject() 
+    void MoveObject()
     {
         heldObject.transform.position = holdArea.position;
     }
 
-    void PickupObject (GameObject pickupObj)
+    void PickupObject(GameObject pickupObj)
     {
         if (pickupObj.GetComponent<Rigidbody>() && !pickupObj.CompareTag("HandLeft") && !pickupObj.CompareTag("HandRight"))
         {
             AudioManager.Instance.ChangerAudio(PickUp);
             rb = pickupObj.GetComponent<Rigidbody>();
             pickupObj.GetComponent<Collider>().enabled = false;
-            
+
             rb.useGravity = false;
             rb.drag = 10;
+
+            if (pickupObj.CompareTag("Whisk"))
+                whisk = true;
+            if (pickupObj.CompareTag("Bowl"))
+                bowl = true;
+
+
 
             rb.transform.parent = holdArea;
             heldObject = pickupObj;
@@ -63,11 +71,16 @@ public class Pickup : MonoBehaviour
     public void DropObject()
     {
         heldObject.GetComponent<Collider>().enabled = true;
-        
+
         rb.useGravity = true;
         rb.drag = 1;
 
         rb.velocity = GetComponent<Rigidbody>().velocity;
+
+        if (heldObject.CompareTag("Whisk"))
+            whisk = false;
+        if (heldObject.CompareTag("Bowl"))
+            bowl = false;
 
         heldObject.transform.parent = null;
         heldObject = null;
@@ -75,42 +88,15 @@ public class Pickup : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Bowl"))
+        if (other.CompareTag("HandRight") || other.CompareTag("HandLeft"))
         {
-            bowl = true;
-            if (whisk && heldObject.CompareTag("Whisk")) 
+            Debug.Log("Arm");
+            if (whisk && otherArm.bowl || bowl && otherArm.whisk)
             {
                 if (_leftArm)
                     minigame.StartMix(true);
                 else
                     minigame.StartMix(false);
-
-
-            }
-        }
-        if (other.CompareTag("Whisk"))
-        {
-            whisk = true;
-            if (bowl && heldObject.CompareTag("Bowl")) 
-            {
-                if (_leftArm)
-                    minigame.StartMix(true);
-                else
-                    minigame.StartMix(false);
-            }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        {
-            if (other.CompareTag("Bowl"))
-            {
-                bowl = false;
-            }
-            if (other.CompareTag("Whisk"))
-            {
-                whisk = false;
             }
         }
     }
